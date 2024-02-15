b46eada07ac49eac87ac8a86af82cdd27aa03569
JimHaff <james.haff@generalassemb.ly>
2024-02-15 17:11:11 -0600

    added the post

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,31 @@
+// import our model so we can talk to the database and perfomr
+// our CRUD operations
+const MovieModel = require('../models/movie')
 
 module.exports = {
-	new: newMovie
+	new: newMovie,
+	create: create
+	// or 
+	// create
+}
+
+
+async function create(req, res){
+	console.log(req.body, " <- is the contents of our form!")
+	req.body.nowShowing = !!req.body.nowShowing // !! forces the value to a boolean
+	  // remove any whitespace at start and end of cast
+	req.body.cast = req.body.cast.trim();
+	  // split cast into an array if it's not an empty string - using a regular expression as a separator
+	if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+
+	try {
+		const createdMovieDoc = await MovieMode.create(req.body)
+		// for now redirect to new page
+		res.redirect('/movies/new')
+	} catch(err){
+		console.log(err)
+		res.redirect('/movies/new')
+	}
 }
 
 function newMovie(req, res){
--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const movieCtrl = require('../controllers/movies')
 
+//get requests to  /movies/new
 router.get('/new', movieCtrl.new)
+//Post request to /movies
+router.post('/', movieCtrl.create)
 
 module.exports = router;
