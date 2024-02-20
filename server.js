a7b4dbd51f6158a7feae580573616585d0ee4120
JimHaff <james.haff@generalassemb.ly>
2024-02-20 14:06:01 -0600

    Added code

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const movieRouter = require('./routes/movies');
+const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
@@ -27,7 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/movies', movieRouter);
-
+// related resources are mounted at /
+// because there is not consitent naming convention
+app.use('/', reviewsRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
