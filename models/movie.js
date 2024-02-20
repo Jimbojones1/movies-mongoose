cf27d7d3e3a38c90480b187df12bbd0906c1ff32
JimHaff <james.haff@generalassemb.ly>
2024-02-20 13:52:57 -0600

    Added

--- models/movie.js
+++ models/movie.js
@@ -5,6 +5,29 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 
+// ONE MOVIE HAS MANY REVIEWS
+// A REVIEW BELONGS TO A MOVIE (Using mongoose embedding to implement the relationship)
+
+// when we use embedding we define the schemas of the relationship in the same file
+// Referencing (each data entity) gets its own model file
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  }
+}, {
+  timestamps: true
+})
+
+
+
+
 // Enforces the shape of the documents (Think of objects)
 // in our mongodb movies collection
 const movieSchema = new Schema({
@@ -12,7 +35,9 @@ const movieSchema = new Schema({
   releaseYear: Number,
   mpaaRating: String,
   cast: [String],
-  nowShowing: Boolean
+  nowShowing: Boolean,
+  // One movie has many reviews, A review belongs to a movie 
+  reviews: [reviewSchema]
 }, {
   timestamps: true
 });
